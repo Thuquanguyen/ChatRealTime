09e18fe3b538ede740bcc49bfa0503a6f76f31fd
quangthu1162 <thunq@rikkeisoft.com>
2020-03-18 11:12:48 +0700

    create project setup Enviroment

---
+++ android/app/src/main/java/test/com/example/flutter_app_chat_realtime/MainActivity.java
@@ -0,0 +1,13 @@
+package test.com.example.flutter_app_chat_realtime;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
